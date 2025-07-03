85d1af8be552481e963b1c27008d5683bece9591
Gabrielfr5491 <gabrielfr3245@gmail.com>
2025-07-03 15:28:52 -0300

    script.js

--- script.js
+++ script.js
@@ -4,16 +4,31 @@ function scrollToSection(sectionId) {
   section.scrollIntoView({ behavior: 'smooth' });
 }
 /* funcao para digitar sozinho na apresentaçao  */
-const text = "Olá, eu sou Gabriel Freire .";
+const text1 = "Olá, eu sou Gabriel Freire.";
+const text2 = "Seja bem-vindo ao meu portfólio!";
 let i = 0;
+let j = 0;
+
 function type() {
-  if (i < text.length) {
-    document.getElementById("typing").innerHTML += text.charAt(i);
+  if (i < text1.length) {
+    document.getElementById("typing").innerHTML += text1.charAt(i);
     i++;
     setTimeout(type, 60);
+  } else {
+    setTimeout(type2, 500); // aguarda um pouco antes de iniciar o segundo
   }
 }
+
+function type2() {
+  if (j < text2.length) {
+    document.getElementById("typing1").innerHTML += text2.charAt(j);
+    j++;
+    setTimeout(type2, 60);
+  }
+}
+
 window.onload = type;
+
 
 
 
